f4dc94af4c416b39af2bcf5cb55e49304bf1a6f0
Linus Mutugi <linusmutugi5178@gmail.com>
2020-03-26 07:48:18 +0300

    changes complete

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -16,13 +16,23 @@ export class GoalComponent implements OnInit {
     new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
     new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
    ];
-
-   toggleDetails(index){
-    this.goals[index].showDescription = !this.goals[index].showDescription;
+   addNewGoal(goal)
+   {
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
   }
-  completeGoal(isComplete, index){
+   toggleDetails(index)
+   {
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+   }
+  deleteGoal(isComplete, index){
       if (isComplete) {
-          this.goals.splice(index,1);
+          let toDelete=confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+          if (toDelete){
+        this.goals.splice(index,1)
+      }
       }
     }
   constructor() { }
